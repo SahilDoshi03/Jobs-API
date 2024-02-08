5854a875dc2597d62f74cdcf9d3708cb74ee34f3
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-02-08 21:22:31 +0530

    Swagger UI Docs Added

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimit = require('express-rate-limit')
 
+// swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 const express = require('express')
 const app = express()
 
@@ -35,8 +41,9 @@ app.use(xss())
 
 
 app.get("/", (req, res) => {
-    res.status(200).send("Jobs API")
+    res.status(200).send("<h1>Jobs API</h1><a href='/api-docs'>API Documentation</a>")
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
